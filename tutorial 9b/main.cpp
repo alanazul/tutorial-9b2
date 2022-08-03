7932cf6e22380e3a1f57387f667c821d00f81b64
50766 <50766@LAPTOP-ERT7LFQ0>
2022-08-03 17:58:39 -0500

    Agregar archivos de proyecto.

---
+++ tutorial 9b/main.cpp
@@ -0,0 +1,11 @@
+//***********************************************//
+//tutorial 9 - github                            //
+//***********************************************//
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hola mundo" << endl;
+	cin.get();
+	return 0;
+}
