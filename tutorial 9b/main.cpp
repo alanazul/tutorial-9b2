d9afe0f8d11269933309655e0d906cba771aaa72
alanazul <alaazulalfa@gmail.com>
2022-08-03 19:52:52 -0500

    Revert "vercion 1.1 sin CIN"
    This reverts commit f6ce87b205c7e93ef454c08c96b4a5c5880e1016.

--- tutorial 9b/main.cpp
+++ tutorial 9b/main.cpp
@@ -1,14 +1,11 @@
 //***********************************************//
-//tutorial 9 - github v1.1                       //
-//***********************************************//
-// ultima modificacion 08/03/2022                //
-// eliminando el CIN                             //
+//tutorial 9 - github                            //
 //***********************************************//
 #include <iostream>
 using namespace std;
 
 int main() {
 	cout << "hola mundo" << endl;
-	
+	cin.get();
 	return 0;
 }
