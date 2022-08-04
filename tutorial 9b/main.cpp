f6ce87b205c7e93ef454c08c96b4a5c5880e1016
alanazul <alaazulalfa@gmail.com>
2022-08-03 19:39:07 -0500

    vercion 1.1 sin CIN

--- tutorial 9b/main.cpp
+++ tutorial 9b/main.cpp
@@ -1,11 +1,14 @@
 //***********************************************//
-//tutorial 9 - github                            //
+//tutorial 9 - github v1.1                       //
+//***********************************************//
+// ultima modificacion 08/03/2022                //
+// eliminando el CIN                             //
 //***********************************************//
 #include <iostream>
 using namespace std;
 
 int main() {
 	cout << "hola mundo" << endl;
-	cin.get();
+	
 	return 0;
 }
